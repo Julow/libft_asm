8be29b1e4565d78c4aa741be9fe06f7f4a66cd43
juloo <juloo.dsi@gmail.com>
2015-01-25 01:17:05 +0100

    Fix on Linux

--- srcs/ft_cat.s
+++ srcs/ft_cat.s
@@ -6,34 +6,40 @@
 ;;   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        ;;
 ;;                                                +#+#+#+#+#+   +#+           ;;
 ;;   Created: 2015/01/22 17:30:19 by jaguillo          #+#    #+#             ;;
-;;   Updated: 2015/01/24 23:34:29 by jaguillo         ###   ########.fr       ;;
+;;   Updated: 2015/01/25 00:16:42 by jaguillo         ###   ########.fr       ;;
 ;;                                                                            ;;
 ;; ************************************************************************** ;;
 
 ; void			ft_cat(int fd);
 global	ft_cat
-extern	ft_puts
+extern	ft_putlstr
 
 ft_cat:
-	mov		r8, rdi		; save rdi
-.loop:
-	; read
+	push	rbx			; save rbx
+	mov		rbx, rdi	; save rdi
+.read:
+%ifdef LINUX
+	mov		rdx, buff_size
+	mov		rcx, buff
+	mov		rax, 3		; read interrupt
+	int		0x80
+%else
 	mov		rdx, buff_size
 	mov		rsi, buff
-	mov		rdi, r8		; fd
-	mov		rax, 0x2000003	; syscall read
-	syscall				; call read
-	jc		.ret		; break loop
+	mov		rdi, rbx
+	mov		rax, 0x2000003	; read syscall
+	syscall
+	jc		.ret		; error break loop
+%endif
 	cmp		rax, 0
-	jle		.ret		; break loop
-	; write
-	mov		rdx, rax
-	mov		rsi, buff
-	mov		rdi, 1
-	mov		rax, 0x2000004	; syscall write
-	syscall				; call write
-	jmp		.loop
+	jle		.ret		; eof break loop
+.write:
+	mov		rsi, rax
+	mov		rdi, buff
+	call	ft_putlstr
+	jmp		.read
 .ret:
+	pop		rbx			; restore rbx
 	ret
 
 section .data
--- srcs/ft_putlstr.s
+++ srcs/ft_putlstr.s
@@ -6,7 +6,7 @@
 ;;   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        ;;
 ;;                                                +#+#+#+#+#+   +#+           ;;
 ;;   Created: 2015/01/24 17:59:24 by jaguillo          #+#    #+#             ;;
-;;   Updated: 2015/01/24 23:32:08 by jaguillo         ###   ########.fr       ;;
+;;   Updated: 2015/01/25 00:05:38 by jaguillo         ###   ########.fr       ;;
 ;;                                                                            ;;
 ;; ************************************************************************** ;;
 
@@ -19,14 +19,14 @@ ft_putlstr:
 	mov		rdx, rsi	; len
 	mov		rcx, rdi	; str
 	mov		rbx, 1		; fd
-	mov		rax, 4
+	mov		rax, 4		; write interrupt
 	int		0x80
 	pop		rbx			; restore rbx
 %else
 	mov		rdx, rsi	; len
 	mov		rsi, rdi	; str
 	mov		rdi, 1		; fd
-	mov		rax, 0x2000004
+	mov		rax, 0x2000004	; write syscall
 	syscall
 %endif
 	mov		rax, 0		; return 0
